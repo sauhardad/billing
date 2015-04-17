cb4e3df29ebf32886b908f48c2ebc30cce02e9f4
unknown <nirdosh_anthem@hotmail.com>
2015-04-17 12:04:09 +0545

    New module level created

--- application/modules/level/controllers/level.php
+++ application/modules/level/controllers/level.php
@@ -10,7 +10,9 @@ class Level extends CI_Controller {
    if(!$this->session->userdata('logged_in'))
        redirect('user/login', 'refresh');
    $this->load->model('level_model');
+   $this->load->model('section/section_model');
    $this->load->helper(array('form'));
+   $this->load->model('subsection/subsection_model');
  }
 
  function index()
@@ -21,22 +23,34 @@ class Level extends CI_Controller {
     if ($session_data['role']=$this->config->item('role_admin'))
         $data['users']=$this->user_model->get_users_except($session_data['id']);
     $data['roles']=$this->config->item('role_value');
-    
+    $data['subsections']=$this->subsection_model->retrieveSubsection();
     $data['levels']=$this->level_model->retrieveLevel();
     $this->template->load('default', 'level/level_main_view',$data);
+  
  }
  
  function add()
  {
-     $data=array();
-     if(($data['code']=$this->input->post('code')) && ($data['name']=$this->input->post('name')) && ($data['type']=$this->input->post('type')))
-     {
-        
-         if($this->level_model->insertLevel($data))
-             echo json_encode(array('status'=>TRUE,'message'=>'Level Saved'));
-         else
-             echo json_encode(array('status'=>FALSE,'message'=>'Oops,try again later'));
-     }
+    $data=array();
+    if(($data['subsection_id']=$this->input->post('add_subsection_dropdown')) && ($data['code']=$this->input->post('add_level_code')) && ($data['name']=$this->input->post('add_level_name')))
+    {
+     if($this->level_model->insertLevel($data))
+         echo json_encode(array('status'=>TRUE,'message'=>'Level Saved'));
+     else
+         echo json_encode(array('status'=>FALSE,'message'=>'Oops,try again later'));
+    }
+ }
+ 
+ function edit()
+ {
+    $data=array();
+    if(($id=$this->input->post('edit_level_id')) && ($data['code']=$this->input->post('edit_level_code')) && ($data['name']=$this->input->post('edit_level_name')) && ($data['subsection_id']=$this->input->post('edit_subsection_dropdown')))
+    {
+     if($this->level_model->updateLevel($id,$data))
+         echo json_encode(array('status'=>TRUE,'message'=>'Level Updated'));
+     else
+         echo json_encode(array('status'=>FALSE,'message'=>'Oops,try again later'));
+    }
  }
  
  function delete()
