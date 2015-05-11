46449b6d011248a885d596f38565aa3d14d90732
unknown <nirdosh_anthem@hotmail.com>
2015-05-11 15:45:46 +0545

    small change in student specific view

--- application/modules/student/views/student_specific_view.php
+++ application/modules/student/views/student_specific_view.php
@@ -2,20 +2,22 @@
         <section style="padding-bottom: 50px; padding-top: 50px;">
             <div class="row">
                 <div class="col-md-4">
-                    <img src="http://localhost/billing/assets/img/user_uploads/ppl.png">
+                    <img src="http://localhost/billing/assets/img/user_uploads"<?php echo $student['photo']; ?>>
                     <br />
                     <br />
+                    
                     <label>Name :<?php echo $student['student_name']; ?></label><br>
                     <label>Address :<?php echo $student['address']; ?></label><br>
                     <label>Contact No :<?php echo $student['contact_no']; ?></label><br>
                     <label>Date of Birth :<?php echo $student['dob']; ?></label><br>
-                    <label>Course Amount:<?php echo $student['total_amount']; ?></label><br>
+                    <label>Total Amount:<?php echo $student['total_amount']; ?></label><br>
                     <br>
                     
                     <br /><br/>
                 </div>
                <div class="col-md-8">
-                   <a href="#" class="btn btn-success">Billing Details</a><br></br>
+                   <h2 style="display:inline-block;">Add Payment</h2>
+                   <button style="margin:1.5%;vertical-align: top;" class="btn" data-toggle="modal" data-target="#add_payment_modal" type="button"> <span class="glyphicon glyphicon-plus"></span></button>
                 <table class="table table-bordered table-striped">
                     <thead>
                         <th>S.N.</th>
@@ -26,17 +28,18 @@
                         <th></th>
                     </thead>
                     <tbody>
-                        <?php if(isset($sections)){ ?>
+                        <?php if(isset($payments)){ ?>
                         <?php $sn=1; ?>
-                        <?php foreach($sections as $section){ ?>
+                        <?php foreach($payments as $payment){ ?>
                             <tr>
                                 <td><?php echo $sn; ?></td>
-                                <td><?php echo $section['code']; ?></td>
-                                <td><?php echo $section['name']; ?></td>
+                                <td><?php echo $payment['bill_no']; ?></td>
+                                <td><?php echo $payment['paid_amount']; ?></td>
+                                <td><?php echo $payment['due_amount']; ?></td>
+                                <td><?php echo $payment['is_running']? "Yes":"No"  ?></td>
                                 <td>
-                                    <a class="btn btn-success" href="<?php echo base_url('section/view/'.$section['id']); ?>"><span class="glyphicon glyphicon-list glyphicon-margin-right-5"></span>View</a>
-                                    <button class="btn btn-primary edit_section_btn" data-id="<?php echo $section['id']; ?>" data-code="<?php echo $section['code']; ?>" data-name="<?php echo $section['name']; ?>" data-toggle="modal" data-target="#edit_section_modal"><span class="glyphicon glyphicon-edit glyphicon-margin-right-5"></span>Edit</button>
-                                    <button class="btn btn-danger" onclick="return deleteData('<?php echo $section['id']; ?>','section/delete',this)"><span class="glyphicon glyphicon-trash glyphicon-margin-right-5"></span>Delete</button>
+                                    <button class="btn btn-primary edit_group_btn" data_bill_no="<?php echo $payment['bill_no']; ?>" data-paid="<?php echo $payment['paid_amount']; ?>" data-due="<?php echo $payment['due_amount']; ?>" data-slot="<?php echo $group['time_slot']; ?>" data-running="<?php echo $group['is_running']; ?>" data-toggle="modal" data-target="#edit__modal"><span class="glyphicon glyphicon-edit glyphicon-margin-right-5"></span>Edit</button>
+                                    <button class="btn btn-danger" onclick="return deleteData('<?php echo $payment['id']; ?>','payment/delete',this)"><span class="glyphicon glyphicon-trash glyphicon-margin-right-5"></span>Delete</button>
                                 </td>
                             </tr> 
                             <?php $sn++; ?>
@@ -47,7 +50,57 @@
             </div>
         </div>
             </div>
-            <!-- ROW END -->
+             <!-- modal for adding Payments -->
+        <div class="modal fade" tabindex="-1" role="dialog" id="add_payment_modal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <a class="close" data-dismiss="modal">×</a>
+                        <h3>Add Payment</h3>
+                    </div>
+                    <div class="modal-body">
+                        <?php echo form_open('payment/add',array('id' => 'add_payment_form')); ?>
+                        <table class="table-padding-10">
+                            <tr>
+                                <td>
+                                    <label for="add_bill_no">Bill Number</label>
+                                </td>
+                                <td colspan="3">
+                                    <input type="text" name="add_bill_no" class="form-control input-sm">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="add_paid_amount">Paid Amount</label>
+                                </td>
+                                <td colspan="3">
+                                    <input type="text" name="add_paid_amount" class="form-control input-sm">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="add_due_amount">Due Amount</label>
+                                </td>
+                                <td colspan="3">
+                                    <input type="text" name="add_due_amount" class="form-control input-sm">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="add_bill_date">Date</label>
+                                </td>
+                                <td>
+                                    <input type="text" name="add_bill_date" class="form-control input-sm datepicker">
+                                </td>
+                        </table>    
+                        
+                        <input class="btn btn-primary" type="submit" value="Save" id="submit">
+                        <?php echo form_close(); ?>
+                        
+                    </div>
+                </div>
+            </div>    
+        </div>
 
 
         </section>
