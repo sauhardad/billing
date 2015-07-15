caa34196edb73123c6656002a82902ece9a77bf5
sauhardad <sauhardad@gmail.com>
2015-07-15 11:26:54 +0545

    New files for receipt

---
+++ application/modules/student/views/receipt.php
@@ -0,0 +1,21 @@
+<div id="printable_div">
+    <span>Valley Institute</span><br/>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    Name:
+                </td>
+                <td>Sauharda Dawadi</td>
+            </tr>
+            <tr>
+                <td>Address:</td>
+                <td>Kalanki,Kathmandu</td>
+            </tr>
+            <tr>
+                <td>Contact NUmber:</td>
+                <td>9841009755</td>
+            </tr>
+        </table>
+    </div>
+</div>
